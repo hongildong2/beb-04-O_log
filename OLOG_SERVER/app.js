0e8e8c36b7a3847363f678814e3a743bfecd5965
mae-zung <candylim97@ewhain.net>
2022-06-22 17:14:55 +0900

    JWT token 구현 완료, 로그인, 로그아웃, post-username 연결 완료

--- OLOG_SERVER/app.js
+++ OLOG_SERVER/app.js
@@ -5,6 +5,7 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 const onchainRouter = require("./router/onchain/index");
 const offchainRouter = require("./router/offchain/index");
+const verifyToken = require("./verifyToken");
 
 const app = express();
 
@@ -19,6 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(verifyToken);
 
 //Offchain middlewares
 // app.use("/onchain", onchainRouter);
--- OLOG_SERVER/router/offchain/auth.js
+++ OLOG_SERVER/router/offchain/auth.js
@@ -4,5 +4,6 @@ const authController = require("../../controller/offchain/authController");
 
 auth.route("/register").post(authController.register);
 auth.route("/login").post(authController.login);
-
+auth.route("/check").get(authController.check);
+auth.route("/logout").post(authController.logout);
 module.exports = auth;
