ac5fa897a8b1772cbc341bac903d3634cc60700c
mae-zung <candylim97@ewhain.net>
2022-06-22 12:13:34 +0900

    posts api 1차 완료

---
+++ OLOG_SERVER/controller/offchain/postsController.js
@@ -0,0 +1,59 @@
+const Post = require("../../models/post");
+
+module.exports = {
+  /*
+    Explore: 모든 posts를 보내줌
+    Get /offchain/posts
+*/
+
+  list: async (req, res) => {
+    try {
+      const posts = await Post.find().exec();
+      res.status(200).send(posts);
+    } catch (e) {
+      res.status(404).send(e);
+    }
+  },
+
+  /*  
+  MyPage: post를 게시함
+  Post /offchain/posts
+  {
+      "blogLink": "블로그링크",
+      "title" : "제목"
+    }
+*/
+
+  write: async (req, res) => {
+    const { blogLink, title } = req.body;
+    const newPost = new Post({
+      blogLink,
+      title,
+    });
+
+    try {
+      await newPost.save();
+      res.status(200).send(newPost);
+    } catch (e) {
+      res.status(404).send(e);
+    }
+  },
+
+  /* 
+    MyPage: 한 명의 유저의 post들을 보내줌
+    Get /offchain/posts/:id 
+*/
+  read: async (req, res) => {
+    const { id } = req.params;
+    try {
+      const post = await Post.findById(id).exec();
+      if (!post) {
+        res.status(404).send("해당 post가 없습니다.");
+        return;
+      }
+      res.status(200).send(post);
+    } catch (e) {
+      res.status(404).send(e);
+    }
+  },
+};
