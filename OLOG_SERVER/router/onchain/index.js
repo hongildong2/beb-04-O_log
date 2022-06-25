980914e90fdf9dddf1a85038f5d9f8ab5a5464b6
yooseungjun <hongildong6987@gmail.com>
2022-06-25 10:57:53 +0900

    ds

--- OLOG_SERVER/router/onchain/index.js
+++ OLOG_SERVER/router/onchain/index.js
@@ -6,8 +6,8 @@ const { walletSync } = require("../../controller/onchain/ERC20controller");
 const express = require("express");
 const router = express.Router();
 
-router.route("/ethFaucet").get(ethFaucet); //서버계정에 1이더씩 던져줌
-router.route("/balance").get(balance); //서버계정의 잔고 조회
+router.route("/ethFaucet").get(ethFaucet); //서버계정에 1이더씩 던져줌 ***테스트용입니다!!
+router.route("/balance").get(balance); //서버계정의 잔고 조회 ***테스트용입니다!!
 router.route("/walletSync").post(walletSync); //지갑 동기화
 
 module.exports = router;
